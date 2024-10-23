22d1a179a31b146510655c9dce197ebd8819fdde
M <markpackham1@gmail.com>
2024-10-23 22:20:17 +0100

    Added myApp.js file

---
+++ myApp.js
@@ -0,0 +1,11 @@
+// 2022 August Git Course
+
+const myApp = {
+  name: "My App",
+  version: "1.0.0",
+  init: function () {
+    console.log(`${this.name} version ${this.version}`);
+  },
+};
+
+myApp.init();
